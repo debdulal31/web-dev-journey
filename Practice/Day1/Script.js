9a38a67a277d8a86abf589ed51507c5787b63898
Debdulal Mahata <76114290+debdulal31@users.noreply.github.com>
2026-03-03 06:40:07 +0000

    just doing basics to get familier with the languagegit add .

--- Practice/Day1/Script.js
+++ Practice/Day1/Script.js
@@ -22,6 +22,94 @@ function factorialOfANumber(n){
     console.log(`The factorial of ${n} is ${factorial}`)
 
 }
+function reverseTheNumber(number){
+    let reverse=0;
+    while(number>0){
+        let digit=number%10;
+        reverse=reverse*10+digit;
+        number=Math.floor(number/10);
+    }
+    console.log(`The reverse of ${number} is ${reverse}`)
+}
+function primeOrNot(number){
+    if(number<=1){
+        console.log(`${number} is not prime`);
+        return;
+    }
+    for(i=2;i<number/2;i++){
+        if (number%i==0) {
+            console.log(`${number} is not a prime number`);
+            return;
+        }
+            
+        
+    }
+    console.log(`${number} is  a prime number`);
+
+}
+function largest(a,b){
+    if (a===b) {
+        console.log("Both are equal")
+    }
+   else if (a>b) {
+        console.log(`${a} is larger than ${b}`);
+    }else{
+        console.log(`${b} is larger than ${a}`);
+    }
+}
+function largestAmongThreeNumber(num1,num2,num3){
+    return (num1>num2?(num1>num3?`${num1} is largest`:`${num3} is largest`):(num2>num3?`${num2} is largest`:`${num3} is largest`))
+}
+// function secondLargestAmongThree(a,b,c){
+//     let max=0;
+//     let secondMax=0;
+//     if (a>=b) {
+//         max=a ;
+//         secondMax=b;
+//         if (b>=c) {
+//             secondMax=b;
+//         }
+//         else{
+//             secondMax=c;
+//         }
+//     }else if (a>=c) {
+//         secondMax=a;
+//     }else if(c>=b){
+
+//         secondMax=b;
+//     }
+//     else{
+//         secondMax=c;
+//     }
+//     console.log(`second larget is ${secondMax}`);
+// }
+
+// function secondLargestAmongThree(a, b, c) {
+
+//     if ((a >= b && a <= c) || (a >= c && a <= b)) {
+//         return a;
+//     }
+
+//     if ((b >= a && b <= c) || (b >= c && b <= a)) {
+//         return b;
+//     }
+
+//     return c;
+// }
+
+function secondLargestAmongThree(a, b, c) {
+    let arr = [a, b, c];
+    arr.sort((x, y) => y - x);
+    return arr[1];
+}
+
 // evenOrOdd(10);
 // sumOfNumbers(10);
 // factorialOfANumber(5);
+//reverseTheNumber(1234);
+// primeOrNot(21);
+// largest(10,20);
+
+// console.log(largestAmongThreeNumber(31,3,7));
+// secondLargestAmongThree(3,10,20);
+console.log("Second largest is", secondLargestAmongThree(10, 13, 20));
